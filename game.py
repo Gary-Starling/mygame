6a5abfd6c9f88c50af0c27a80785d83169e7e286
Gary-Starling <skvorec92@mail.ru>
2022-04-28 22:12:30 +0300

    my_save

--- game.py
+++ game.py
@@ -5,14 +5,10 @@ import pygame
 import os
 
 
-
-game = True
-
+game = True #Флаг бесконечного цикла игры
 
 ''' Настройки экрана'''
-
-
-class window_settings:
+class Window_settings:
     WIDTH = 800  # ширина игрового окна
     HEIGHT = 600  # высота игрового окна
     FPS = 30  # частота кадров в секунду
@@ -22,22 +18,21 @@ class window_settings:
 colors = {"BLACK": [0, 0, 0], "WHITE": [255, 255, 255]}
 
 
-''' Класс создаваемого объекта '''
-class some_obj(pygame.sprite.Sprite):
+''' Класс создаваемого объекта игрока'''
+class Player(pygame.sprite.Sprite):
+
+    #флаги движения < >
+    left = False
+    right = False 
+
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = player_img
+        self.images = [] = player_img
         self.rect = self.image.get_rect()
-        self.rect.center = (400, 120)
+        self.rect.center = (100, 500)
         self.speedx = 0
 
     def update(self):
-        '''
-        #Ушли за левый край
-        if self.rect.right < 0:
-            self.rect.right = window_settings.WIDTH 
-            '''
-
         self.speedx = 0
         keystate = pygame.key.get_pressed()
         if keystate[pygame.K_LEFT]:
@@ -48,44 +43,56 @@ class some_obj(pygame.sprite.Sprite):
         self.rect.x += self.speedx   
 
         #Ушли за правый край
-        if self.rect.left > window_settings.WIDTH:
+        if self.rect.left > Window_settings.WIDTH:
             self.rect.right = 0
 
         if self.rect.right < 0: 
-            self.rect.left = window_settings.WIDTH    
+            self.rect.left = Window_settings.WIDTH    
 
 
 
- # настройка папки ассетов
+""" START """
 
-pygame.init()  # Инит драйвера
+pygame.init()        # Инит драйвера
 pygame.mixer.init()  # для звука
-window = pygame.display.set_mode((window_settings.WIDTH, window_settings.HEIGHT))  # Разрешение
 
+window = pygame.display.set_mode((Window_settings.WIDTH, Window_settings.HEIGHT))  # Разрешение
+
+''' Папки '''
+#TODO: Сделать правильные пути
 game_folder = os.path.dirname(__file__)
 img_folder = os.path.join(game_folder, 'img')
-player_img = pygame.image.load(os.path.join(img_folder, 'mario.png')).convert()
+
+# Нулевые координаты для фоновой картинки
+background_position = [0, 0]
+
+
+#TODO: Загрузи все картинки RAZBERI KLASSI
+player_img = pygame.image.load(os.path.join(img_folder, 'mario.png')).convert() #Игрок
+background_image = pygame.image.load(os.path.join(img_folder, 'bg.png')).convert() #задний фон
 
 
 pygame.display.set_caption("Run Gary! RUN!") # Заголовок окна
+
 clock = pygame.time.Clock()
 window.fill(colors["WHITE"])
 
 
 # Спрайты
 sprites = pygame.sprite.Group()
-rect = some_obj()
-sprites.add(rect)
+hero = Player()
+sprites.add(hero)
 
 while game:
     # Обновление
     sprites.update()
     
     # Рендеринг
-    window.fill(colors["WHITE"])
+    window.blit(background_image, background_position)
+    #window.fill(colors["WHITE"])
     sprites.draw(window)
 
-    clock.tick(window_settings.FPS)
+    clock.tick(Window_settings.FPS)
 
     for event in pygame.event.get():
         # проверить закрытие окна
@@ -93,9 +100,9 @@ while game:
             game = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                rect.speedx = -8
+                hero.speedx = -8
             if event.key == pygame.K_RIGHT:
-                rect.speedx = 8    
+                hero.speedx = 8    
 
     pygame.display.update()
 
