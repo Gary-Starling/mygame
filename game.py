25f23c927534b699fd8bfbb75fe9b2af37775df9
Gary-Starling <skvorec92@mail.ru>
2022-05-02 21:51:11 +0300

    new

--- game.py
+++ game.py
@@ -1,78 +1,105 @@
-# my first game
+# The mushrooms
 
+from pickle import FALSE
 import random
 import pygame
 import os
+
+''' CONST '''
+WIDTH = 800  # ширина игрового окна
+HIGHT = 600  # высота игрового окна
+FPS = 30  # частота кадров в секунду
+
 
 
 game = True #Флаг бесконечного цикла игры
 
 ''' Настройки экрана'''
 class Window_settings:
-    WIDTH = 800  # ширина игрового окна
-    HEIGHT = 600  # высота игрового окна
-    FPS = 30  # частота кадров в секунду
+    # Конструктор
+    def __init__(self, width, hight, fps):
+        self.width = width# ширина игрового окна
+        self.hight = hight  # высота игрового окна
+        self.fps = fps # частота кадров в секунду
 
 
 ''' Доступные цвета '''
 colors = {"BLACK": [0, 0, 0], "WHITE": [255, 255, 255]}
 
 
-''' Класс создаваемого объекта игрока'''
-class Player(pygame.sprite.Sprite):
+''' Класс создаваемого игрока'''
+class Player(pygame.sprite.Sprite): #Родительский класс Sprite
 
-    #флаги движения < >
+    # флаги движения
     left = False
     right = False 
+    up = False
+    down = False
 
-    def __init__(self):
+    # Конструктор
+    def __init__(self, image_name):
         pygame.sprite.Sprite.__init__(self)
-        self.images = [] = player_img
+        '''установив методом convert_alpha необходимую прозрачность '''
+        self.index = 0
+        self.images = []
+        self.images.append(pygame.image.load(os.path.join(img_folder, "mush_stay.png")).convert_alpha())  #Игрок
+        self.images.append(pygame.image.load(os.path.join(img_folder, "mush_l1.png")).convert_alpha())
+        self.images.append(pygame.image.load(os.path.join(img_folder, "mush_l2.png")).convert_alpha())
+
+        self.image = self.images[self.index]
         self.rect = self.image.get_rect()
         self.rect.center = (100, 500)
         self.speedx = 0
 
+    # Обновление движения
     def update(self):
         self.speedx = 0
         keystate = pygame.key.get_pressed()
+
         if keystate[pygame.K_LEFT]:
             self.speedx = -8
+            self.index += 1
+            if self.index >= len(self.images):
+                self.index = 0
+        self.image = self.images[self.index]
 
         if keystate[pygame.K_RIGHT]:
             self.speedx = 8
         self.rect.x += self.speedx   
 
         #Ушли за правый край
-        if self.rect.left > Window_settings.WIDTH:
+        if self.rect.left > settings.width:
             self.rect.right = 0
 
         if self.rect.right < 0: 
-            self.rect.left = Window_settings.WIDTH    
+            self.rect.left = settings.width    
 
 
 
 """ START """
+settings = Window_settings(WIDTH, HIGHT, FPS) # Настройки экрана
 
 pygame.init()        # Инит драйвера
 pygame.mixer.init()  # для звука
 
-window = pygame.display.set_mode((Window_settings.WIDTH, Window_settings.HEIGHT))  # Разрешение
+
+window = pygame.display.set_mode((settings.width, settings.hight))  # Создать окно с разр. X x Y
 
 ''' Папки '''
 #TODO: Сделать правильные пути
 game_folder = os.path.dirname(__file__)
-img_folder = os.path.join(game_folder, 'img')
+bg_folder = os.path.join(game_folder, 'img')
+img_folder = os.path.join(game_folder, 'img/mush_left')
 
 # Нулевые координаты для фоновой картинки
 background_position = [0, 0]
 
 
-#TODO: Загрузи все картинки RAZBERI KLASSI
-player_img = pygame.image.load(os.path.join(img_folder, 'mario.png')).convert() #Игрок
-background_image = pygame.image.load(os.path.join(img_folder, 'bg.png')).convert() #задний фон
+#TODO: Загрузи все картинки
+background_image = pygame.image.load(os.path.join(bg_folder, 'bg.png')).convert() #задний фон
 
 
-pygame.display.set_caption("Run Gary! RUN!") # Заголовок окна
+pygame.display.set_caption("MUSHROOMS") # Заголовок окна
 
 clock = pygame.time.Clock()
 window.fill(colors["WHITE"])
@@ -80,7 +107,7 @@ window.fill(colors["WHITE"])
 
 # Спрайты
 sprites = pygame.sprite.Group()
-hero = Player()
+hero = Player('mush_stay.png')
 sprites.add(hero)
 
 while game:
@@ -89,10 +116,9 @@ while game:
     
     # Рендеринг
     window.blit(background_image, background_position)
-    #window.fill(colors["WHITE"])
     sprites.draw(window)
 
-    clock.tick(Window_settings.FPS)
+    clock.tick(settings.fps)
 
     for event in pygame.event.get():
         # проверить закрытие окна
